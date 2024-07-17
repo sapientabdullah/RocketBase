ff9858eb9d9fb8d5df78b8d1401fd781b415f8fc
Abdullah <sapientabdullah@gmail.com>
2024-07-17 09:00:23 +0500

    Propulsion System

--- PropulsionSystem.lua
+++ PropulsionSystem.lua
@@ -178,3 +178,41 @@ SN4Ignition:GetPropertyChangedSignal("Value"):Connect(function()
 		end
 	end
 end)
+SN5Ignition:GetPropertyChangedSignal("Value"):Connect(function()
+	if SN5Ignition.Value == true then
+		--propulsion on
+		for _,engine in pairs(script.Parent:GetChildren()) do
+			if engine.Name == ("Raptor Engine") then
+				if engine:FindFirstChild("SerialNumber").Value == 5 then
+					for _,thrust in pairs(engine:GetChildren()) do
+						if thrust.Name == ("Thrust") then
+							thrust.Sound:Play()
+							thrust.Light.Enabled = true
+							thrust.Afterburner.Enabled = true
+							game:GetService("RunService").Heartbeat:Connect(function()
+								thrust.VectorForce.Force = Vector3.new(0,-vacuum_thrust,0)
+							end)
+							thrust.VectorForce.Enabled = true
+						end
+					end
+				end
+			end
+		end
+	elseif SN5Ignition.Value == false then
+		--propulsion off
+		for _,engine in pairs(script.Parent:GetChildren()) do
+			if engine.Name == ("Raptor Engine") then
+				if engine:FindFirstChild("SerialNumber").Value == 5 then
+					for _,thrust in pairs(engine:GetChildren()) do
+						if thrust.Name == ("Thrust") then
+							thrust.Sound:Stop()
+							thrust.Light.Enabled = false
+							thrust.Afterburner.Enabled = false
+							thrust.VectorForce.Enabled = false
+						end
+					end
+				end
+			end
+		end
+	end
+end)
