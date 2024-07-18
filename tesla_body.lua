843db99b4fc3c983c5d3fc33e2ff30a2da0ebe26
Abdullah <sapientabdullah@gmail.com>
2024-07-18 16:18:10 +0500

    Tesla Script

--- tesla_body.lua
+++ tesla_body.lua
@@ -11,3 +11,40 @@ for _,x in pairs(script.Parent:GetDescendants()) do
 		x:SetNetworkOwner(nil)
 	end
 end
+game:GetService("RunService").Heartbeat:Connect(function()
+	for _,seat in pairs(script.Parent:GetChildren()) do
+		if seat:IsA("Seat") then
+			local occupant = nil
+			if seat.Occupant ~= nil then
+				occupant = seat.Occupant
+				if occupant ~= nil then
+					for _,part in pairs(seat.Occupant.Parent:GetDescendants()) do
+						if part:IsA("BasePart") or part:IsA("MeshPart") then
+							local PhysProperties = PhysicalProperties.new(0,0.3,0,1,0)
+							part.CustomPhysicalProperties = PhysProperties
+							part.Massless = true
+						end
+					end
+				elseif seat.Occupant == nil then
+					occupant = nil
+				end
+			end
+		end
+	end
+	for _,character in pairs(workspace:GetChildren()) do
+		if character:FindFirstChild("Humanoid") then
+			if character.Humanoid.Jump == true then
+				delay(0.5, function()
+					for _,part in pairs(character:GetDescendants()) do
+						if part:IsA("BasePart") or part:IsA("MeshPart") then
+							local PhysProperties = PhysicalProperties.new(0.7,0.3,0,1,0)
+							part.CustomPhysicalProperties = PhysProperties
+							part.Massless = false
+						end
+					end
+				end)
+			end
+		end
+	end
+end)
+
