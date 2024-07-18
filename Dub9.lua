059505bf97029548bab16936e8de7b248d1021d0
Abdullah <sapientabdullah@gmail.com>
2024-07-18 09:51:04 +0500

    Dub 9

--- Dub9.lua
+++ Dub9.lua
@@ -39,3 +39,22 @@ local function explode()
 		
 	end	
 end
+
+shared.ExplodeCheck = game:GetService("RunService").Stepped:Connect(function()
+	if exploded == false then
+			local cx,cy,cz = math.floor(Body.Velocity.X),math.floor(Body.Velocity.Y),math.floor(Body.Velocity.Z)
+			local current_velocity = Vector3.new(cx,cy,cz)
+			--body portion
+			for _,body in pairs(script.Parent:GetDescendants()) do
+				if body.Name == ("ShipBody") then
+					body.Touched:Connect(function(Obj)
+						if Obj.Size.X > Body.Size.Y or Obj.Size.Y > Body.Size.Y or Obj.Size.Z > Body.Size.Y and Obj.Name ~= "Ray" and Obj.Name ~= "Sensor" then
+							if cx >= configuration["BodyExplosionVelocity"] or cy >= configuration["BodyExplosionVelocity"] or cz >= configuration["BodyExplosionVelocity"] then
+								explode()
+							end
+						end
+					end)
+				end
+			end
+	end
+end)
